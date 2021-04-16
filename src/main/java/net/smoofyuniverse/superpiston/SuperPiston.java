59918b579a57c6e61a44bbcb70886e00cbd8727b
Yeregorix <yeregorixcraft@gmail.com>
2021-04-17 01:27:00 +0200

    Use OreUpdater 1.0.1

--- src/main/java/net/smoofyuniverse/superpiston/SuperPiston.java
+++ src/main/java/net/smoofyuniverse/superpiston/SuperPiston.java
@@ -23,12 +23,8 @@
 package net.smoofyuniverse.superpiston;
 
 import com.google.inject.Inject;
-import net.smoofyuniverse.ore.OreAPI;
-import net.smoofyuniverse.ore.project.OreProject;
-import net.smoofyuniverse.ore.project.OreVersion;
-import net.smoofyuniverse.superpiston.config.global.GlobalConfig;
+import net.smoofyuniverse.ore.update.UpdateChecker;
 import net.smoofyuniverse.superpiston.config.world.WorldConfig;
-import net.smoofyuniverse.superpiston.event.PlayerEventListener;
 import net.smoofyuniverse.superpiston.event.WorldEventListener;
 import net.smoofyuniverse.superpiston.impl.internal.InternalServer;
 import net.smoofyuniverse.superpiston.util.IOUtil;
@@ -38,7 +34,6 @@ import ninja.leaping.configurate.commented.CommentedConfigurationNode;
 import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 import ninja.leaping.configurate.loader.ConfigurationLoader;
 import ninja.leaping.configurate.objectmapping.GuiceObjectMapperFactory;
-import ninja.leaping.configurate.objectmapping.ObjectMappingException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongepowered.api.Game;
@@ -49,23 +44,15 @@ import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
 import org.spongepowered.api.event.game.state.GameStartedServerEvent;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.plugin.PluginContainer;
-import org.spongepowered.api.scheduler.Task;
-import org.spongepowered.api.text.Text;
-import org.spongepowered.api.text.action.TextActions;
-import org.spongepowered.api.text.format.TextColors;
 import org.spongepowered.api.world.World;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
-import static java.lang.Math.max;
 import static net.smoofyuniverse.superpiston.util.MathUtil.clamp;
 
 @Plugin(id = "superpiston", name = "SuperPiston", version = "1.0.7", authors = "Yeregorix", description = "Allows to modify vanilla pistons")
@@ -87,11 +74,6 @@ public class SuperPiston {
 	private Path worldConfigsDir;
 
 	private final Map<String, WorldConfig.Immutable> configs = new HashMap<>();
-	private GlobalConfig.Immutable globalConfig;
-
-	private OreAPI oreAPI;
-	private OreProject oreProject;
-	private Text[] updateMessages = new Text[0];
 
 	public SuperPiston() {
 		if (instance != null)
@@ -108,52 +90,16 @@ public class SuperPiston {
 		}
 		this.configOptions = ConfigurationOptions.defaults().withObjectMapperFactory(this.factory);
 
-		LOGGER.info("Loading global configuration ..");
-		try {
-			loadGlobalConfig();
-		} catch (Exception ex) {
-			LOGGER.error("Failed to load global configuration", ex);
-		}
-
 		this.game.getEventManager().registerListeners(this, new WorldEventListener());
-		this.game.getEventManager().registerListeners(this, new PlayerEventListener());
+
+		this.game.getEventManager().registerListeners(this, new UpdateChecker(LOGGER, this.container,
+				createConfigLoader(this.configDir.resolve("update.conf")), "Yeregorix", "SuperPiston"));
 	}
 
 	@Listener
 	public void onGameReload(GameReloadEvent e) {
 		this.configs.clear();
 		this.game.getServer().getWorlds().forEach(this::loadConfig);
-	}
-
-	public void loadGlobalConfig() throws IOException, ObjectMappingException {
-		if (this.globalConfig != null)
-			throw new IllegalStateException("Config already loaded");
-
-		Path file = this.configDir.resolve("global.conf");
-		ConfigurationLoader<CommentedConfigurationNode> loader = createConfigLoader(file);
-
-		CommentedConfigurationNode root = loader.load();
-		int version = root.getNode("Version").getInt();
-		if ((version > GlobalConfig.CURRENT_VERSION || version < GlobalConfig.MINIMUM__VERSION) && IOUtil.backupFile(file)) {
-			LOGGER.info("Your global config version is not supported. A new one will be generated.");
-			root = loader.createEmptyNode();
-		}
-
-		ConfigurationNode cfgNode = root.getNode("Config");
-		GlobalConfig cfg = cfgNode.getValue(GlobalConfig.TOKEN, new GlobalConfig());
-
-		cfg.updateCheck.repetitionInterval = max(cfg.updateCheck.repetitionInterval, 0);
-		cfg.updateCheck.consoleDelay = clamp(cfg.updateCheck.consoleDelay, -1, 100);
-		cfg.updateCheck.playerDelay = clamp(cfg.updateCheck.playerDelay, -1, 100);
-
-		if (cfg.updateCheck.consoleDelay == -1 && cfg.updateCheck.playerDelay == -1)
-			cfg.updateCheck.enabled = false;
-
-		root.getNode("Version").setValue(GlobalConfig.CURRENT_VERSION);
-		cfgNode.setValue(GlobalConfig.TOKEN, cfg);
-		loader.save(root);
-
-		this.globalConfig = cfg.toImmutable();
 	}
 
 	public void loadConfig(World world) {
@@ -199,52 +145,6 @@ public class SuperPiston {
 			LOGGER.info("SuperPiston " + this.container.getVersion().orElse("?") + " was loaded successfully.");
 		else
 			LOGGER.error("!!WARNING!! SuperPiston was not loaded correctly. Be sure that the jar file is at the root of your mods folder!");
-
-		if (this.globalConfig.updateCheck.enabled) {
-			this.oreAPI = new OreAPI();
-			this.oreProject = new OreProject("superpiston");
-			this.oreProject.setNamespace("Yeregorix", "SuperPiston");
-			Task.builder().async().interval(this.globalConfig.updateCheck.repetitionInterval, TimeUnit.HOURS).execute(this::checkForUpdate).submit(this);
-		}
-	}
-
-	public void checkForUpdate() {
-		String version = this.container.getVersion().orElse(null);
-		if (version == null)
-			return;
-
-		LOGGER.debug("Checking for update ..");
-
-		OreVersion latestVersion = null;
-		try {
-			latestVersion = OreVersion.getLatest(this.oreProject.getVersions(this.oreAPI), v -> v.apiVersion.charAt(0) == '7').orElse(null);
-		} catch (Exception e) {
-			LOGGER.info("Failed to check for update", e);
-		}
-
-		if (latestVersion != null && !latestVersion.name.equals(version)) {
-			Text msg1 = Text.join(Text.of("A new version of SuperPiston is available: "),
-					Text.builder(latestVersion.name).color(TextColors.AQUA).build(),
-					Text.of(". You're currently using version: "),
-					Text.builder(version).color(TextColors.AQUA).build(),
-					Text.of("."));
-
-			Text msg2;
-			try {
-				msg2 = Text.builder("Click here to open the download page.").color(TextColors.GOLD)
-						.onClick(TextActions.openUrl(new URL(latestVersion.getPage()))).build();
-			} catch (MalformedURLException e) {
-				msg2 = null;
-			}
-
-			if (this.globalConfig.updateCheck.consoleDelay != -1) {
-				Task.builder().delayTicks(this.globalConfig.updateCheck.consoleDelay)
-						.execute(() -> this.game.getServer().getConsole().sendMessage(msg1)).submit(this);
-			}
-
-			if (this.globalConfig.updateCheck.playerDelay != -1)
-				this.updateMessages = msg2 == null ? new Text[]{msg1} : new Text[]{msg1, msg2};
-		}
 	}
 
 	public ConfigurationLoader<CommentedConfigurationNode> createConfigLoader(Path file) {
@@ -253,16 +153,6 @@ public class SuperPiston {
 
 	public Optional<WorldConfig.Immutable> getConfig(World world) {
 		return Optional.ofNullable(this.configs.get(world.getName()));
-	}
-
-	public GlobalConfig.Immutable getGlobalConfig() {
-		if (this.globalConfig == null)
-			throw new IllegalStateException("Config not loaded");
-		return this.globalConfig;
-	}
-
-	public Text[] getUpdateMessages() {
-		return this.updateMessages;
 	}
 
 	public PluginContainer getContainer() {
